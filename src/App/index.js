2c2e7b3811525e683b6ae2be96de5cdad1de2ba1
Faisal <faisalhmohd@gmail.com>
2020-05-22 10:13:28 +0600

    Add section one

--- src/App/index.js
+++ src/App/index.js
@@ -10,6 +10,10 @@ function App() {
       <div className="window">
         <div className="window-inner">
           <WindowComponents />
+          <div className="section-one">
+            <h2>Mohammad Faisal</h2>
+            <h1>Software and Media Engineer, Race Driver based from Dhaka, Bangladesh.</h1>
+          </div>
         </div>
       </div>
     </div>
