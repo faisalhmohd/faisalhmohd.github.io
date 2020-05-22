0536ef75211b709e97cf37cb8c0128f52df48958
Faisal <faisalhmohd@gmail.com>
2020-05-22 11:37:34 +0600

    Add cursor component to app with mercedes image

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 
 import WindowComponents from './WindowComponents';
+import Cursor from './Cursor';
 
 import './App.sass';
+import Mercedes from './mercedes.jpg';
 
 function App() {
   return (
     <div className="App">
+      <Cursor/>
       <div className="window">
         <div className="window-inner">
           <WindowComponents />
           <div className="section-one">
             <h2>Mohammad Faisal</h2>
             <h1>Software and Media Engineer, Race Driver based from Dhaka, Bangladesh.</h1>
+          </div>
+          <div className="section-two">
+            <h2>Portfolio</h2>
+            <h1>Some work I have contributed to.</h1>
+
+            <div className="portfolio">
+              <div className="portfolio-item">
+                <img src={Mercedes} alt="Mercedes Work" />
+              </div>
+              <div className="portfolio-item">
+                <img src={Mercedes} alt="Mercedes Work" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
