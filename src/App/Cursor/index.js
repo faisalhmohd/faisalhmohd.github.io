767ac4bfde7ce744f8f58c4781976bcba9600172
Faisal <faisalhmohd@gmail.com>
2020-05-22 11:37:22 +0600

    Add cursor component

---
+++ src/App/Cursor/index.js
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from 'react';
+
+function Cursor() {
+  const cursorRef = useRef(null);
+
+  
+
+  useEffect(() => {
+    const handleCursorPosition = e => {
+      const xPos = e.clientX;
+      const yPos = e.clientY;
+      
+      cursorRef.current.style.top = yPos + "px";
+      cursorRef.current.style.left = xPos + "px";
+    }
+    window.addEventListener('mousemove', handleCursorPosition);
+
+    return () => {
+      window.removeEventListener('mousemove', handleCursorPosition);
+    }
+  }, []);
+
+  return (
+    <div className="cursor" ref={cursorRef} />
+  )
+}
+
+export default Cursor;
