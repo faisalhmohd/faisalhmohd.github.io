80215a3ca63b1f3122606a7b65aa66ae6804b6ca
Faisal <faisalhmohd@gmail.com>
2020-05-22 09:33:19 +0600

    Add logo

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import Logo from './Logo.jpg'
+import './App.sass';
 
 function App() {
   return (
     <div className="App">
       <div className="window">
-        <div className="window-inner"></div>
+        <div className="window-inner">
+          <div className="window-components">
+            <div className="logo">
+              <img src={Logo} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
