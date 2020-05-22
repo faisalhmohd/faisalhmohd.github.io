ca0266e3eb5be25940c5762a29301488e14ff049
Faisal <faisalhmohd@gmail.com>
2020-05-22 22:30:59 +0600

    Precise cursor position

--- src/App/Cursor/index.js
+++ src/App/Cursor/index.js
@@ -1,5 +1,8 @@
 import React, { useRef, useEffect } from 'react';
 
+const CURSOR_WIDTH = 100;
+const CURSOR_HEIGHT = 100;
+
 function Cursor() {
   const cursorRef = useRef(null);
 
@@ -8,10 +11,10 @@ function Cursor() {
       const xPos = e.clientX;
       const yPos = e.clientY;
       
-      cursorRef.current.style.top = yPos + "px";
-      cursorRef.current.style.left = xPos + "px";
-      cursorRef.current.style.height = "100px";
-      cursorRef.current.style.width = "100px";
+      cursorRef.current.style.top = yPos - CURSOR_HEIGHT/2 + "px";
+      cursorRef.current.style.left = xPos - CURSOR_WIDTH/2 + "px";
+      cursorRef.current.style.height = CURSOR_HEIGHT + "px";
+      cursorRef.current.style.width = CURSOR_WIDTH + "px";
     };
     const handleCursorOutDisplay = e => {
       const xPos = e.clientX;
