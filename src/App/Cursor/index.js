4b729340257426c61ca4deba00ff010818bf59cf
Faisal <faisalhmohd@gmail.com>
2020-05-22 22:23:23 +0600

    Remove cursor when outside screen

--- src/App/Cursor/index.js
+++ src/App/Cursor/index.js
@@ -10,11 +10,22 @@ function Cursor() {
       
       cursorRef.current.style.top = yPos + "px";
       cursorRef.current.style.left = xPos + "px";
-    }
+      cursorRef.current.style.height = "100px";
+      cursorRef.current.style.width = "100px";
+    };
+    const handleCursorOutDisplay = e => {
+      const xPos = e.clientX;
+      const yPos = e.clientY;
+      
+      cursorRef.current.style.height = "0px";
+      cursorRef.current.style.width = "0px";
+    };
     window.addEventListener('mousemove', handleCursorPosition);
+    window.addEventListener('mouseout', handleCursorOutDisplay);
 
     return () => {
       window.removeEventListener('mousemove', handleCursorPosition);
+      window.removeEventListener('mouseout', handleCursorOutDisplay);
     }
   }, []);
 
