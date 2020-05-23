41b2d8ba9766f321104e4dc467e8ef9f3125e8ca
Faisal <faisalhmohd@gmail.com>
2020-05-24 00:38:32 +0600

    Change to default width and height of cursor

--- src/App/Cursor/index.js
+++ src/App/Cursor/index.js
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect, useContext } from 'react';
 import { MouseAreaContext } from '../Contexts/MouseAreaProvider';
 
-const CURSOR_WIDTH = 100;
-const CURSOR_HEIGHT = 100;
+const CURSOR_WIDTH = 20;
+const CURSOR_HEIGHT = 20;
 
 function Cursor() {
   const cursorRef = useRef(null);
@@ -15,15 +15,13 @@ function Cursor() {
       
       cursorRef.current.style.top = yPos - CURSOR_HEIGHT/2 + "px";
       cursorRef.current.style.left = xPos - CURSOR_WIDTH/2 + "px";
-      cursorRef.current.style.height = CURSOR_HEIGHT + "px";
-      cursorRef.current.style.width = CURSOR_WIDTH + "px";
     };
     const handleCursorOutDisplay = e => {
       const xPos = e.clientX;
       const yPos = e.clientY;
       
-      cursorRef.current.style.height = "0px";
-      cursorRef.current.style.width = "0px";
+      // cursorRef.current.style.height = "0px";
+      // cursorRef.current.style.width = "0px";
     };
     window.addEventListener('mousemove', handleCursorPosition);
     window.addEventListener('mouseout', handleCursorOutDisplay);
