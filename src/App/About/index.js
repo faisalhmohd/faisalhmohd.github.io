ee7976ee667d7dc53e04dcffd2b78a0563e7f1fd
Faisal <faisalhmohd@gmail.com>
2020-05-24 00:38:42 +0600

    Plug mouse area props to about section

--- src/App/About/index.js
+++ src/App/About/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MouseAreaContext } from '../Contexts/MouseAreaProvider';
 
 function About() {
+  const { getMouseAreaProps} = useContext(MouseAreaContext);
+
   return (
-    <div className="about-section">
+    <div className="about-section" {...getMouseAreaProps('about-section')}>
       <h2>Mohammad Faisal</h2>
       <h1>Software and Media Engineer, Race Driver based from Dhaka, Bangladesh.</h1>
     </div>
