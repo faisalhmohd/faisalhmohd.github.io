caa3e45b195923737b95ddbf9344941f88d31853
Faisal <faisalhmohd@gmail.com>
2020-05-23 23:42:30 +0600

    Move About to its own component

---
+++ src/App/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="section-one">
+      <h2>Mohammad Faisal</h2>
+      <h1>Software and Media Engineer, Race Driver based from Dhaka, Bangladesh.</h1>
+    </div>
+  );
+}
+
+export default About;
