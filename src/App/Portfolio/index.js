edb286c43e003dc81008355061c71e4fcf31990f
Faisal <faisalhmohd@gmail.com>
2020-05-24 08:31:03 +0600

    Add animation over portfolio heading

--- src/App/Portfolio/index.js
+++ src/App/Portfolio/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CursorAnimationContext } from '../Contexts/CursorAnimationProvider';
 import Mercedes from '../mercedes.jpg';
 
 function Portfolio() {
+  const CursorAnimation = useContext(CursorAnimationContext);
+ 
   return (
     <div className="portfolio-section">
-      <h2>Portfolio</h2>
-      <h1>Some work I have contributed to.</h1>
+      <div className="portfolio-section-heading"  {...CursorAnimation.getProps('portfolio-section-heading')}>
+        <h2>Portfolio</h2>
+        <h1>Some work I have contributed to.</h1>
+      </div>
 
       <div className="portfolio">
         <div className="portfolio-item">
