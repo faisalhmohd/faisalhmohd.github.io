10788433f81352722f11e4f2da3214e920ece180
Faisal <faisalhmohd@gmail.com>
2020-05-24 10:51:51 +0600

    Display conditional white logo

--- src/App/WindowComponents/index.js
+++ src/App/WindowComponents/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Logo from '../Logo.jpg'
+import Logo from '../Logo.png';
+import LogoWhite from '../Logo-White.png';
 
-function WindowComponents() {
+function WindowComponents({ currentSection }) {
+  const isLogoWhite = ['portfolio-section-mercedes'].includes(currentSection);
+
   return (
     <div className="window-components">
       <div className="logo">
-        <img src={Logo} alt="logo" />
+        <img src={isLogoWhite ? LogoWhite : Logo} alt="logo" />
       </div>
       <div className="navbar">
         {["about", "work", "contact"].map((item) => (
