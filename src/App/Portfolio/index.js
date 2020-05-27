38c2530badcf5faedffa45835d9a2f6d2db73491
Faisal <faisalhmohd@gmail.com>
2020-05-28 01:19:10 +0600

    Add slideshow for mercedes portfolio section

--- src/App/Portfolio/index.js
+++ src/App/Portfolio/index.js
@@ -1,27 +1,46 @@
 import React, { useContext } from 'react';
 import { CursorAnimationContext } from '../Contexts/CursorAnimationProvider';
-import Mercedes from '../portfolio-mercedes.jpg';
-import Emirates from '../portfolio-emirates.png';
+import MercedesAMGOne from './mercedes-amg-photo-1.png';
+import MercedesAMGTwo from './mercedes-amg-photo-2.png';
+import MercedesAMGThree from './mercedes-amg-photo-3.png';
+import MercedesAMGLogo from './mercedes-amg.png';
+import EmiratesSkyCargoLogo from './emirates-sky-cargo.png';
+
 
 function Portfolio() {
   const CursorAnimation = useContext(CursorAnimationContext);
+
+  const mercedesAmgImages = [
+    'https://www.mercedes-benz.com/en/mbsocialcar/mbsocialcar-mercedes-amg-gt-r/_jcr_content/root/slider_6/sliderchilditems/slideritem_3/image/MQ7-0-image-20190114132747/4-mercedes-benz-vehicles-mercedes-amg-gt-r-c-190-3400x1440.jpeg',
+    MercedesAMGOne,
+    MercedesAMGTwo,
+    MercedesAMGThree
+  ];
  
   return (
     <div className="portfolio-section">
       <div className="portfolio-section-heading"  {...CursorAnimation.getProps('portfolio-section-heading')}>
         <h2>Portfolio</h2>
-        <h1>Some work I have contributed to.</h1>
+        <h1>Selected work I have contributed to.</h1>
       </div>
 
       <div className="portfolio">
         <div className="portfolio-item" {...CursorAnimation.getProps('portfolio-section-mercedes')}>
           <a href="https://www.mercedes-amg.com/en.html" target="_blank">
-            <img src={Mercedes} alt="Mercedes-AMG Work" />
+            <div className="slideshow-wrapper">
+              {mercedesAmgImages.map(image => (
+                <div className="slide">
+                  <img src={image} className="background-image" alt="Mercedes-AMG Work" />
+                </div>
+              ))}
+            </div>
+            <img className="logo" src={MercedesAMGLogo} />
           </a>
         </div>
         <div className="portfolio-item" {...CursorAnimation.getProps('portfolio-section-emirates')}>
           <a href="https://www.skycargo.com" target="_blank">
-            <img src={Emirates} alt="Emirates SkyCargo Work" />
+            <img src="https://www.arabianbusiness.com/public/images/2020/03/25/Emirates-SkyCargo.jpg" className="background-image" alt="Emirates SkyCargo Work" />
+            <img className="logo" src={EmiratesSkyCargoLogo} />
           </a>
         </div>
       </div>
