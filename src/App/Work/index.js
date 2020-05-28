5e3588f6a5081dfd0ce477a54589d2a5c73c6c00
Faisal <faisalhmohd@gmail.com>
2020-05-28 07:57:31 +0600

    Add all work items

--- src/App/Work/index.js
+++ src/App/Work/index.js
@@ -2,6 +2,57 @@ import React, { useContext } from 'react';
 import { CursorAnimationContext } from '../Contexts/CursorAnimationProvider';
 import Link from '../Link';
 
+const workItems = [
+  {
+    link: "https://www.crbnd.com/",
+    company: 'CRBND',
+    title: 'Co-Founder & CTO',
+    time: '2017 - Present'
+  },
+  {
+    link: "https://www.newscred.com/",
+    company: 'NewsCred',
+    title: 'Software Engineer',
+    time: '2017 - Present'
+  },
+  {
+    link: "https://www.mercedesamgf1.com/en/",
+    company: 'Mercedes-AMG Petronas F1 Team',
+    title: 'Media Engineer',
+    time: '2013 - Present'
+  },
+  {
+    link: "https://codesign.com.bd",
+    company: 'CoDesign',
+    title: 'Technology Lead',
+    time: '2016 - 2017'
+  },
+  {
+    link: "https://webable.digital",
+    company: 'WebAble Digital',
+    title: 'Lead Developer',
+    time: '2014 - 2016'
+  },
+  {
+    link: "https://www.emirates.com",
+    company: 'Emirates',
+    title: 'Aircraft Maintenance Engineer',
+    time: '2012 - 2013'
+  },
+  {
+    link: "https://www.yasmarinacircuit.com",
+    company: 'Yas Marina Circuit',
+    title: 'Team Principal & Race Driver',
+    time: '2012 - 2019'
+  },
+  {
+    link: "https://mod.gov.ae/",
+    company: 'UAE Armed Forces',
+    title: 'Junior Corporal',
+    time: '2011 - 2012'
+  }
+];
+
 function Work() {
   const CursorAnimation = useContext(CursorAnimationContext);
 
@@ -12,22 +63,16 @@ function Work() {
         <h1>Amazing Teams I have been a part of.</h1>
       </div>
 
-      <div className="work-section-items">
-        <div className="work-section-item" {...CursorAnimation.getProps('work-section-item')}>
-          <Link href="https://www.newscred.com/">
-            <h1 className="company-name">NewsCred</h1>
-            <h1>Software Engineer</h1>
-            <h2>2019 - Present</h2>
-          </Link>
-        </div>
-
-        <div className="work-section-item"  {...CursorAnimation.getProps('work-section-item')}>
-          <Link href="https://www.mercedesamgf1.com/en/">
-            <h1  className="company-name">Mercedes-AMG Petronas F1 Team</h1>
-            <h1>Media Engineer</h1>
-            <h2>2019 - Present</h2>
-          </Link>
-        </div>
+      <div className="work-section-items" {...CursorAnimation.getProps('work-section-item')}>
+        {workItems.map(work => (
+          <div className="work-section-item" >
+            <Link href={work.link}>
+              <h1 className="company-name">{work.company}</h1>
+              <h1>{work.title}</h1>
+              <h2>{work.time}</h2>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
